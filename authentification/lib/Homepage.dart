7b4602f17264600809340e57d0c134904c195dc6
Firas001 <firas.elhag@gmail.com>
2021-08-16 21:47:34 +0300

    fixed errors and syntax of the code and also added a homepage after login is done but still not functional

---
+++ authentification/lib/Homepage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- authentification/lib/login.dart
+++ authentification/lib/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' show FirebaseAuth;
+import 'package:authentification/Homepage.dart';
 
 class login extends StatefulWidget {
   @override
@@ -8,12 +9,12 @@ class login extends StatefulWidget {
 
 class _loginState extends State<login> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  final GlobalKey<FormState> formkey = GlobalKey<FormState>();
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
 
-  String _email, _password;
+  late String _email, password;
 
   CheckAuthenification() async {
-    _auth.onAuthStateChanged.listen((user) {
+    _auth.authStateChanges().listen((user) {
       if (user != null) {
         Navigator.push(
             context, MaterialPageRoute(builder: (context) => Homepage()));
@@ -29,6 +30,65 @@ class _loginState extends State<login> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        color: Colors.yellow[50],
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 400,
+              child: Image(
+                image: AssetImage(""),
+                fit: BoxFit.contain,
+              ),
+            ),
+            Container(
+              child: Form(
+                key: _formkey,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      child: TextFormField(validator: (input) {
+                        if (input!.isNotEmpty) return 'Enter Email';
+
+                        decoration:
+                        InputDecoration(
+                            labelText: 'Email', prefixIcon: Icon(Icons.email));
+                        onSaved:
+                        (input) => _email = input;
+                      }),
+                    ),
+                    Container(
+                      child: TextFormField(validator: (input) {
+                        if (input!.length < 6)
+                          return 'provide a min of 6 character';
+
+                        decoration:
+                        InputDecoration(
+                            labelText: 'Password',
+                            prefixIcon: Icon(Icons.lock));
+                        obSecureText:
+                        true;
+
+                        onSaved:
+                        (input) => _email = input;
+                      }),
+                    ),
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Text('Login',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold)),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
